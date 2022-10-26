6a037cdb2c5b7e954dfcd1bb5d7896b2496216fa
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-10-26 21:52:45 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -27,5 +27,6 @@ function calculate() {
   const area = [(baseA + baseB) / 2] * height
 
   // output
-  document.getElementById("area").innerHTML = "The area is: " + area.toFixed(2) + "cm"
+  document.getElementById("area").innerHTML =
+    "The area is: " + area.toFixed(2) + "cm"
 }
